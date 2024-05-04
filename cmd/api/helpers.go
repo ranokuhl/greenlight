daa3ffef3469df430d4ad1c5d853dd4c80b5f5e4
Rano Kuhl <46578162+ranokuhl@users.noreply.github.com>
2024-05-04 21:27:56 +0200

    sending json responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,53 +1,49 @@
 package main
 
 import (
-    "errors"
-    "net/http"
-    "strconv"
-    "encoding/json"
-    "github.com/julienschmidt/httprouter"
+	"encoding/json"
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
 )
 
 // Retrieve the "id" URL parameter from the current request context, then convert it to
-// an integer and return it. If the operation isn't successful, return 0 and an error. 
+// an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
-    params := httprouter.ParamsFromContext(r.Context())
+	params := httprouter.ParamsFromContext(r.Context())
 
-    id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-    if err != nil || id < 1 {
-        return 0, errors.New("invalid id parameter")
-    }
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
 
-    return id, nil
+	return id, nil
 }
 
+// Define an envelope type.
+type envelope map[string]any
+
 // Define a writeJSON() helper for sending responses. This takes the destination
-// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a 
+// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
 // header map containing any additional HTTP headers we want to include in the response.
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-    // Encode the data to JSON, returning the error if there was one.
-    js, err := json.Marshal(data)
-    if err != nil {
-        return err
-    }
+	// Use the json.MarshalIndent() function so that whitespace is added to the encoded
+	// JSON. Here we use no line prefix ("") and tab indents ("\t") for each element.
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
 
-    // Append a newline to make it easier to view in terminal applications.
-    js = append(js, '\n')
+	js = append(js, '\n')
 
-    // At this point, we know that we won't encounter any more errors before writing the
-    // response, so it's safe to add any headers that we want to include. We loop
-    // through the header map and add each header to the http.ResponseWriter header map.
-    // Note that it's OK if the provided header map is nil. Go doesn't throw an error
-    // if you try to range over (or generally, read from) a nil map.
-    for key, value := range headers {
-        w.Header()[key] = value
-    }
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
 
-    // Add the "Content-Type: application/json" header, then write the status code and
-    // JSON response.
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(status)
-    w.Write(js)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
 
-    return nil
+	return nil
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,40 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strconv"
+	"fmt"
+	"net/http"
+	"time"
 
-    "github.com/julienschmidt/httprouter"
+	"greenlight/internal/data"
 )
 
-// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply 
+// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
 // return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "create a new movie")
+	fmt.Fprintln(w, "create a new movie")
 }
 
-// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve 
-// the interpolated "id" parameter from the current URL and include it in a placeholder 
+// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
+// the interpolated "id" parameter from the current URL and include it in a placeholder
 // response.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-    // When httprouter is parsing a request, any interpolated URL parameters will be
-    // stored in the request context. We can use the ParamsFromContext() function to
-    // retrieve a slice containing these parameter names and values.
-    params := httprouter.ParamsFromContext(r.Context())
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
 
-    // We can then use the ByName() method to get the value of the "id" parameter from 
-    // the slice. In our project all movies will have a unique positive integer ID, but 
-    // the value returned by ByName() is always a string. So we try to convert it to a 
-    // base 10 integer (with a bit size of 64). If the parameter couldn't be converted, 
-    // or is less than 1, we know the ID is invalid so we use the http.NotFound() 
-    // function to return a 404 Not Found response.
-    id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-    if err != nil || id < 1 {
-        http.NotFound(w, r)
-        return
-    }
+	// Create a new instance of the Movie struct, containing the ID we extracted from
+	// the URL and some dummy data. Also notice that we deliberately haven't set a
+	// value for the Year field.
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
 
-    // Otherwise, interpolate the movie ID in a placeholder response.
-    fmt.Fprintf(w, "show the details of movie %d\n", id)
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
+	}
 }
