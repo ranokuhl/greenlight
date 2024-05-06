39971216e46e60171466e944fd3889762c6156d9
Rano Kuhl <46578162+ranokuhl@users.noreply.github.com>
2024-05-06 10:41:33 +0200

    advanced crud operations

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -28,6 +28,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	// Add the route for the PUT /v1/movies/:id endpoint.
 	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
+	// Require a PATCH request, rather than PUT.
+	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	// Add the route for the DELETE /v1/movies/:id endpoint.
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
